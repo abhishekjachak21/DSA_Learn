df37abb3164198f866f3e0e595e10ce66725fc0d
Abhishek Jachak <abhishekjachak22@gmail.com>
2024-02-17 14:52:28 +0530

    sortRev

--- SearchSort/BubSort.cc
+++
@@ -1,5 +0,0 @@
-#include<iostream>
-using namespace std ;
-
-int main()
- i AM THE STUDENT OF PCCOE COLLEGE  
--- SearchSort/Selesortt.cc
+++ SearchSort/Selesortt.cc
@@ -2,44 +2,74 @@
 using namespace std;
 // #include<bits/stdc++.h>
 
-void selectionSort(int arr[], int n );
-void print(int arr[], int n);
-
-int main()
-{
-    // int arr[] = {5,4,22,10,1,6,100,89,2};
-    int arr[] = {5,8,4,9,2};
-    int i,j,n,temp;
-    n = sizeof(arr)/sizeof(arr[0]);
-    //n = 9 ; 
-
-    cout<<"Unsorted Array :";print(arr,n);
-    // sort(arr,arr+n);
-    selectionSort(arr,n);
-    cout<<"Sorted Array :"; print(arr,n);
-
-    return(0);
-}
-
-void print(int arr[], int n)
-{
-    for(int i=0;i<n;i++)
-        cout<<arr[i]<<" ";
-    cout<<endl;
-}
-
-void selectionSort(int arr[], int n )
-{
-    int i,j,min_in;
-    for(i=0;i<n;i++)
-    {
-      min_in = i;
-        for(j=i+1;j<n;j++)
-            if (arr[j] < arr[min_in])
-                min_in = j;
-       swap(arr[i],arr[min_in]);
+void selectSort(int arr[]){
+ int n=sizeof(arr)/sizeof(arr[0]);
+   for(int i=0;i<n;i++){
+    int min_idx=i;
+    for(int j=i+1;j<n;j++){
+      if(arr[j]<arr[min_idx]){ 
+        j=min_idx;
+        swap(arr[j],arr[min_idx]);
     }
+   }
+
+  for(int i:arr) cout<<i<<" ";
+
+
 }
+
+
+int main(){
+
+    int arr[]={9,3,-5,1,8};
+
+cout<<"Unsorted arr: ";
+    for(int i:arr) cout<<i<<" ";
+
+cout<<"Sorted arr: ";
+    selectSort(arr);
+    
+
+}
+
+
+// void print(int arr[], int n)
+// {
+//     for(int i=0;i<n;i++)
+//         cout<<arr[i]<<" ";
+//     cout<<endl;
+// }
+
+// void selectionSort(int arr[], int n )
+// {
+//     int i,j,min_in;
+//     for(i=0;i<n;i++)
+//     {
+//       min_in = i;
+//         for(j=i+1;j<n;j++)
+//             if (arr[j] < arr[min_in])
+//                 min_in = j;
+//        swap(arr[i],arr[min_in]);
+//     }
+// }
+
+// int main()
+// {
+//     // int arr[] = {5,4,22,10,1,6,100,89,2};
+//     int arr[] = {5,8,4,9,2};
+//     int i,j,n,temp;
+//     n = sizeof(arr)/sizeof(arr[0]);
+//     //n = 9 ; 
+
+//     cout<<"Unsorted Array :";print(arr,n);
+//     // sort(arr,arr+n);
+//     selectionSort(arr,n);
+//     cout<<"Sorted Array :"; print(arr,n);
+
+//     return(0);
+// }
+
+
 
 
 
