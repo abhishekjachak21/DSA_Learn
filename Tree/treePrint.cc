e5cf513cc4c2c948709db0afe3833e9f52afb3b5
Abhishek Jachak <abhishekjachak22@gmail.com>
2024-08-08 01:03:01 +0530

    fought fear,did tree today, solved 4 problems

---
+++ Tree/treePrint.cc
@@ -0,0 +1,68 @@
+#include <iostream>
+using namespace std;
+
+
+  // Create a sample tree:
+    //         1
+    //        / \
+    //       2   3
+    //      / \   \
+    //     4   5   6
+
+
+// Definition of TreeNode
+class TreeNode {
+    public:
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+// Inorder Traversal
+void inorderTraversal(TreeNode* root) {
+    if (root == NULL) return;
+    inorderTraversal(root->left);
+    cout << root->val << " ";
+    inorderTraversal(root->right);
+}
+
+// Preorder Traversal
+void preorderTraversal(TreeNode* root) {
+    if (root == NULL) return;
+    cout << root->val << " ";
+    preorderTraversal(root->left);
+    preorderTraversal(root->right);
+}
+
+// Postorder Traversal
+void postorderTraversal(TreeNode* root) {
+    if (root == NULL) return;
+    postorderTraversal(root->left);
+    postorderTraversal(root->right);
+    cout << root->val << " ";
+}
+
+int main() {
+  
+    TreeNode* root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(3);
+    root->left->left = new TreeNode(4);
+    root->left->right = new TreeNode(5);
+    root->right->right = new TreeNode(6);
+
+    cout << "Inorder Traversal: ";
+    inorderTraversal(root);
+    cout << endl;
+
+    cout << "Preorder Traversal: ";
+    preorderTraversal(root);
+    cout << endl;
+
+    cout << "Postorder Traversal: ";
+    postorderTraversal(root);
+    cout << endl;
+
+    return 0;
+}
